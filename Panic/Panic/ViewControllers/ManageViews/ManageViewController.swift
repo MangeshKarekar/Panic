33bc576e8d8adf3ecbf8c167ad969ab7fc815503
Mangesh <Mangesh@Mangeshs-MacBook-Pro.local>
2017-10-24 11:50:14 +0100

    UI improvements

--- Panic/Panic/ViewControllers/ManageViews/ManageViewController.swift
+++ Panic/Panic/ViewControllers/ManageViews/ManageViewController.swift
@@ -80,6 +80,10 @@ class ManageViewController: UIViewController,UITableViewDataSource,UITableViewDe
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.performSegue(withIdentifier: contactsSegue, sender: colorResults?[indexPath.section])
     }
